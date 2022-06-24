78228607d82d4d58a7a1591321aa2f5b4d8532e8
Brian <brian@briandenicola.com>
2022-06-24 20:02:19 +0000

    Stand alone cluster

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,10 +3,23 @@
 # this runs at Codespace creation - not part of pre-build
 echo "$(date)    post-create start" >> ~/status
 
+echo "$(date)    Create Flink minikube cluster" >> ~/status
 minikube start --kubernetes-version=v1.21.5 --force
 kubectl create -f https://github.com/jetstack/cert-manager/releases/download/v1.7.1/cert-manager.yaml
-helm repo add flink-operator-repo https://downloads.apache.org/flink/flink-kubernetes-operator-1.0.0/
-helm install flink-kubernetes-operator flink-operator-repo/flink-kubernetes-operator
-kubectl get pods
+#helm repo add flink-operator-repo https://downloads.apache.org/flink/flink-kubernetes-operator-1.0.0/
+#helm install flink-kubernetes-operator flink-operator-repo/flink-kubernetes-operator
+#kubectl create -f https://raw.githubusercontent.com/apache/flink-kubernetes-operator/release-1.0/examples/basic.yaml
+#kubectl port-forward svc/basic-example-rest 8081 
+#kubectl get pods
+
+echo "$(date)    Create stand-alone Flink cluster" >> ~/status
+wget https://dlcdn.apache.org/flink/flink-1.15.0/flink-1.15.0-bin-scala_2.12.tgz
+tar xzf flink-*.tgz
+rm -f flink-*.tgz
+cd flink*
+./bin/start-cluster.sh
+
+#./bin/flink run examples/streaming/WordCount.jar
+#tail log/flink-*-taskexecutor-*.out
 
 echo "$(date)    post-create complete" >> ~/status
