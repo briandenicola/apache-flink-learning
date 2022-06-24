9e449acd695bdaad48649b4d852984f849ede57a
Brian <brian@briandenicola.com>
2022-06-24 20:27:30 +0000

    Fixed K8S cluster configuration

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,11 +6,11 @@ echo "$(date)    post-create start" >> ~/status
 echo "$(date)    Create Flink minikube cluster" >> ~/status
 minikube start --kubernetes-version=v1.21.5 --force
 kubectl create -f https://github.com/jetstack/cert-manager/releases/download/v1.7.1/cert-manager.yaml
-#helm repo add flink-operator-repo https://downloads.apache.org/flink/flink-kubernetes-operator-1.0.0/
-#helm install flink-kubernetes-operator flink-operator-repo/flink-kubernetes-operator
-#kubectl create -f https://raw.githubusercontent.com/apache/flink-kubernetes-operator/release-1.0/examples/basic.yaml
+helm repo add flink-operator-repo https://downloads.apache.org/flink/flink-kubernetes-operator-1.0.0/
+helm install flink-kubernetes-operator flink-operator-repo/flink-kubernetes-operator
+kubectl create -f https://raw.githubusercontent.com/apache/flink-kubernetes-operator/release-1.0/examples/basic.yaml
+kubectl get pods
 #kubectl port-forward svc/basic-example-rest 8081 
-#kubectl get pods
 
 echo "$(date)    Create stand-alone Flink cluster" >> ~/status
 wget https://dlcdn.apache.org/flink/flink-1.15.0/flink-1.15.0-bin-scala_2.12.tgz
