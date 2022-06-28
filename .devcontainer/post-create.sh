0e7da7dd0a20f3884ca4fcfb4f8b441a2691c161
Brian <brian@briandenicola.com>
2022-06-28 15:31:50 +0000

    Added minikube start online to post-start

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,17 +9,10 @@ kubectl create -f https://github.com/jetstack/cert-manager/releases/download/v1.
 helm repo add flink-operator-repo https://downloads.apache.org/flink/flink-kubernetes-operator-1.0.0/
 helm install flink-kubernetes-operator flink-operator-repo/flink-kubernetes-operator
 kubectl create -f https://raw.githubusercontent.com/apache/flink-kubernetes-operator/release-1.0/examples/basic.yaml
-kubectl get pods
-#kubectl port-forward svc/basic-example-rest 8081 
 
-echo "$(date)    Create stand-alone Flink cluster" >> ~/status
+echo "$(date)    Download flink" >> ~/status
 wget https://dlcdn.apache.org/flink/flink-1.15.0/flink-1.15.0-bin-scala_2.12.tgz
 tar xzf flink-*.tgz
 rm -f flink-*.tgz
-cd flink*
-./bin/start-cluster.sh
-
-#./bin/flink run examples/streaming/WordCount.jar
-#tail log/flink-*-taskexecutor-*.out
 
 echo "$(date)    post-create complete" >> ~/status
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -9,4 +9,15 @@ echo alias k=kubectl >> ~/.bashrc
 echo 'source <(kubectl completion bash)' >> ~/.bashrc 
 echo complete -F __start_kubectl k >> ~/.bashrc
 
+echo "$(date)    Create Flink minikube cluster" >> ~/status
+minikube start --kubernetes-version=v1.21.5 --force
+kubectl get pods
+kubectl port-forward svc/basic-example-rest 8081 
+
+#echo "$(date)    Create stand-alone Flink cluster" >> ~/status
+#cd flink*
+#./bin/start-cluster.sh
+#./bin/flink run examples/streaming/WordCount.jar
+#tail log/flink-*-taskexecutor-*.out
+
 echo "$(date)    post-start complete" >> ~/status
