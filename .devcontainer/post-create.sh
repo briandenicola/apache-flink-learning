75b4abbfe24cbeec27e8986a8702a4f284c2ae7a
Brian <brian@briandenicola.com>
2022-06-28 20:52:25 +0000

    Added python

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -18,4 +18,7 @@ wget https://dlcdn.apache.org/flink/flink-1.15.0/flink-1.15.0-bin-scala_2.12.tgz
 tar xzf flink-*.tgz
 rm -f flink-*.tgz
 
+echo "$(date)    Install Python Flink libraries" >> ~/status
+python -m pip install apache-flink
+
 echo "$(date)    post-create complete" >> ~/status
