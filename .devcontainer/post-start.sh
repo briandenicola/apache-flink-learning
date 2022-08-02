23b4f3380131486436a6d7bf91c0da294c6aa57c
Brian <brian@briandenicola.com>
2022-08-02 15:49:44 +0000

    Updated documentation

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,6 +5,7 @@
 echo "$(date)    post-start start" >> ~/status
 
 echo "$(date)    Setting bashrc" >> ~/status
+
 echo . /etc/bash_completion >> ~/.bashrc
 echo alias k=kubectl >> ~/.bashrc
 echo 'source <(kubectl completion bash)' >> ~/.bashrc 
@@ -12,16 +13,5 @@ echo complete -F __start_kubectl k >> ~/.bashrc
 
 echo "$(date)    Create Flink minikube cluster" >> ~/status
 minikube start --kubernetes-version=v1.21.5 --force
-#kubectl create sa flink
-#kubectl create rolebinding flink-operator --clusterrole=flink-operator --serviceaccount=default:flink
-#kubectl create -f https://raw.githubusercontent.com/apache/flink-kubernetes-operator/release-1.0.1/examples/basic.yaml
-#kubectl get pods
-#kubectl port-forward svc/basic-example-rest 8081 
-
-#echo "$(date)    Create stand-alone Flink cluster" >> ~/status
-#cd flink*
-#./bin/start-cluster.sh
-#./bin/flink run examples/streaming/WordCount.jar
-#tail log/flink-*-taskexecutor-*.out
 
 echo "$(date)    post-start complete" >> ~/status
--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Overview
-## Components 
-## Architecture
 
-# Infrastructure
-## Prerequisites
-## Deployment 
+This repository is intended as a place of learning flink utilizing CodeSpaces and DevContainers
+_More details pending_
 
-# Application Deployment 
-## Validate 
+# Quickstart
 
-# Backlog 
+## Stand-alone Cluster
+cd flink*
+./bin/start-cluster.sh
+./bin/flink run examples/streaming/WordCount.jar
+tail log/flink-*-taskexecutor-*.out
+
+## Kubernetes Cluster
+kubectl create sa flink
+kubectl create rolebinding flink-operator --clusterrole=flink-operator --serviceaccount=default:flink
+kubectl create -f https://raw.githubusercontent.com/apache/flink-kubernetes-operator/release-1.0.1/examples/basic.yaml
+kubectl get pods
