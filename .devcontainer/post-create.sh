14de7c96060f082b1bc6627f8555b59c419138d3
briandenicola <brian@briandenicola.com>
2023-02-24 15:39:56 -0600

    Removed minikube cluster

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,24 +3,6 @@
 # this runs at Codespace creation - not part of pre-build
 echo "$(date)    post-create start" >> ~/status
 
-echo "$(date)    Create envsubst" >> ~/status
-curl -Lso envsubst https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-Linux-x86_64
-sudo install envsubst /usr/local/bin
-rm -rf ./envsubst
-
-echo "$(date)    Create Flink minikube cluster" >> ~/status
-minikube start --kubernetes-version=v1.24.3 --force
-
-kubectl create sa flink
-kubectl create rolebinding flink-operator --clusterrole=flink-operator --serviceaccount=default:flink
-
-helm repo add flink-operator-repo https://downloads.apache.org/flink/flink-kubernetes-operator-1.1.0
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-
-helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.8.2  --set installCRDs=true --wait
-helm upgrade -i flink-kubernetes-operator flink-operator-repo/flink-kubernetes-operator --namespace flink --create-namespace
-
 echo "$(date)    Download flink" >> ~/status
 wget https://dlcdn.apache.org/flink/flink-1.15.0/flink-1.15.0-bin-scala_2.12.tgz
 tar xzf flink-*.tgz
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,14 +4,7 @@
 
 echo "$(date)    post-start start" >> ~/status
 
-echo "$(date)    Setting bashrc" >> ~/status
-
-echo . /etc/bash_completion >> ~/.bashrc
-echo alias k=kubectl >> ~/.bashrc
-echo 'source <(kubectl completion bash)' >> ~/.bashrc 
-echo complete -F __start_kubectl k >> ~/.bashrc
-
-echo "$(date)    Create Flink minikube cluster" >> ~/status
-minikube start --kubernetes-version=v1.24.35 --force
+#echo "$(date)    Create Flink minikube cluster" >> ~/status
+#minikube start --kubernetes-version=v1.24.35 --force
 
 echo "$(date)    post-start complete" >> ~/status
